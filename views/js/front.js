478f9296cebe0cb2a5d0641f8146a58658db358a
aleksandr_raskopin <aleksandr@involix.com>
2021-05-08 08:59:27 +0300

    front.js fixed.

--- views/js/front.js
+++ views/js/front.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
-    $('#payment-confirmation button').on('click', function(e) {
-        if ($('input[name="payment-option"]:checked').attr('data-module-name') === 'mercury_cash') {
+    $("#payment-confirmation button").on("click", function(e) {
+        if ($('input[name="payment-option"]:checked').attr("data-module-name") === "mercury_cash") {
             e.preventDefault();
-            $('body').prepend('<div id="mercury-cash"></div>');
+            $("body").prepend('<div id="mercury-cash"></div>');
 
             var url              = $('input[name="url"]').val();
             var status_url       = $('input[name="status_url"]').val();
@@ -14,8 +14,8 @@ $(document).ready(function () {
             $.ajax({
                 ajax: 1,
                 url: get_settings_url,
-                type: 'post',
-                dataType: 'json',
+                type: "post",
+                dataType: "json",
                 success(data) {
                     var price        = data.price;
                     var currency     = data.currency;
@@ -28,8 +28,8 @@ $(document).ready(function () {
                         checkoutUrl: url,
                         statusUrl: status_url,
                         checkStatusInterval: refresh_period,
-                        mount: '#mercury-cash',
-                        lang: 'en',
+                        mount: "#mercury-cash",
+                        lang: "en",
                         limits: {
                             BTC:  minimum_btc,
                             ETH:  minimum_eth,
@@ -37,69 +37,69 @@ $(document).ready(function () {
                         }
                     });
                     sdk.checkout(price, currency, email);
-                    sdk.on('close', (obj) => {
-                        if(obj.status && obj.status === 'TRANSACTION_APROVED') {
-                            $('body').addClass('loading');
+                    sdk.on("close", (obj) => {
+                        if(obj.status && obj.status === "TRANSACTION_APROVED") {
+                            $("body").addClass("loading");
                             $.ajax({
                                 ajax: 1,
                                 url: success_url,
-                                type: 'post',
-                                dataType: 'json',
+                                type: "post",
+                                dataType: "json",
                                 success(data) {
-                                    $('body').removeClass('loading');
+                                    $("body").removeClass("loading");
                                     if (data.result == true) {
                                         var url = data.url;
                                         window.location.href = url;
                                     } else {
-                                        $('#mercury-cash').remove();
-                                        $('#errorModalLabel').html(data.error);
-                                        $('#errorModal').modal('toggle');
+                                        $("#mercury-cash").remove();
+                                        $("#errorModalLabel").html(data.error);
+                                        $("#errorModal").modal("toggle");
                                     }
                                 },
                                 error(jqXHR, exception) {
-                                    $('body').removeClass('loading');
+                                    $("body").removeClass("loading");
                                     var msg = '';
                                     if (jqXHR.status === 0) {
-                                        msg = 'Not connect.\n Verify Network.';
-                                    } else if (jqXHR.status == 404) {
-                                        msg = 'Requested page not found. [404]';
-                                    } else if (jqXHR.status == 500) {
-                                        msg = 'Internal Server Error [500].';
-                                    } else if (exception === 'parsererror') {
-                                        msg = 'Requested JSON parse failed.';
-                                    } else if (exception === 'timeout') {
-                                        msg = 'Time out error.';
-                                    } else if (exception === 'abort') {
-                                        msg = 'Ajax request aborted.';
+                                        msg = "Not connect.\n Verify Network.";
+                                    } else if (jqXHR.status === 404) {
+                                        msg = "Requested page not found. [404]";
+                                    } else if (jqXHR.status === 500) {
+                                        msg = "Internal Server Error [500].";
+                                    } else if (exception === "parsererror") {
+                                        msg = "Requested JSON parse failed.";
+                                    } else if (exception === "timeout") {
+                                        msg = "Time out error.";
+                                    } else if (exception === "abort") {
+                                        msg = "Ajax request aborted.";
                                     } else {
                                         msg = 'Uncaught Error.\n' + jqXHR.responseText;
                                     }
-                                    $('#errorModalLabel').html(msg);
-                                    $('#errorModal').modal('toggle');
+                                    $("#errorModalLabel").html(msg);
+                                    $("#errorModal").modal("toggle");
                                 }
                             });
                         }
                     });
                 },
                 error(jqXHR, exception) {
-                    var msg = '';
+                    var msg = "";
                     if (jqXHR.status === 0) {
-                        msg = 'Not connect.\n Verify Network.';
-                    } else if (jqXHR.status == 404) {
-                        msg = 'Requested page not found. [404]';
-                    } else if (jqXHR.status == 500) {
-                        msg = 'Internal Server Error [500].';
-                    } else if (exception === 'parsererror') {
-                        msg = 'Requested JSON parse failed.';
-                    } else if (exception === 'timeout') {
-                        msg = 'Time out error.';
-                    } else if (exception === 'abort') {
-                        msg = 'Ajax request aborted.';
+                        msg = "Not connect.\n Verify Network.";
+                    } else if (jqXHR.status === 404) {
+                        msg = "Requested page not found. [404]";
+                    } else if (jqXHR.status === 500) {
+                        msg = "Internal Server Error [500].";
+                    } else if (exception === "parsererror") {
+                        msg = "Requested JSON parse failed.";
+                    } else if (exception === "timeout") {
+                        msg = "Time out error.";
+                    } else if (exception === "abort") {
+                        msg = "Ajax request aborted.";
                     } else {
                         msg = 'Uncaught Error.\n' + jqXHR.responseText;
                     }
-                    $('#errorModalLabel').html(msg);
-                    $('#errorModal').modal('toggle');
+                    $("#errorModalLabel").html(msg);
+                    $("#errorModal").modal("toggle");
                 }
             });
             return false;
